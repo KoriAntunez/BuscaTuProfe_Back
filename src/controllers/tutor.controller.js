8748c916abaa9dde8003f6aee596564a5cf00f6c
KoriAntunez <kori.antunez@unmsm.edu.pe>
2022-11-17 12:05:35 -0500

    commit

--- src/controllers/tutor.controller.js
+++ src/controllers/tutor.controller.js
@@ -1,52 +1,66 @@
-const fs = require('fs-extra');
+const fs = require("fs-extra");
 
 // Importando modelos
-const Tutor = require('../models/tutor');
+const Tutor = require("../models/tutor");
 
 // Método para crear o actualizar el perfil del tutor
 const createTutor = async (req, res) => {
-    const {
-        name,
-        surname,
-        country,
-        city,
-        biography,
-        network
-    } = req.body;
+  const { name, surname, country, city, biography, network } = req.body;
 
-    // Buscamos al tutor y actualizamos sus datos si es que existe
-    let tutor = await Tutor.findOneAndUpdate({ user_id: req.params.id }, { 
-        name,
-        surname,
-        country,
-        city,
-        biography,
-        network }, { new: true });
-    let msgResp = "Perfil de tutor actualizado exitosamente.";
+  // Buscamos al tutor y actualizamos sus datos si es que existe
+  let tutor = await Tutor.findOneAndUpdate(
+    { user_id: req.params.id },
+    {
+      name,
+      surname,
+      country,
+      city,
+      biography,
+      network,
+    },
+    { new: true }
+  );
+  let msgResp = "Perfil de tutor actualizado exitosamente.";
 
-    // Si es que el tutor no existe
-    if (!tutor) {
-        const newTutor = new Tutor({
-            name,
-            surname,
-            country,
-            city,
-            biography,
-            network,
-            user_id: req.params.id
-        });
-        // Guardamos el perfil del tutor en la BD
-        tutor = await newTutor.save();
-        msgResp = "Perfil de tutor creado exitosamente.";
-    }
-
-    res.status(200).json({
-        tutor,
-        msg: msgResp
+  // Si es que el tutor no existe
+  if (!tutor) {
+    const newTutor = new Tutor({
+      name,
+      surname,
+      country,
+      city,
+      biography,
+      network,
+      user_id: req.params.id,
     });
+    // Guardamos el perfil del tutor en la BD
+    tutor = await newTutor.save();
+    msgResp = "Perfil de tutor creado exitosamente.";
+  }
+
+  res.status(200).json({
+    tutor,
+    msg: msgResp,
+  });
 };
 
+// Método para mostrar perfil de tutor
+const viewTutor = async (req, res) => {
+  const tutor = await Tutor.findById(req.params.id);
+
+  // Si es que el tutor no existe
+  if (!tutor)
+    return res.status(400).json({
+      msg: "Tutor no existe.",
+    });
+
+  res.status(200).json({
+    tutor,
+    msg: "Perfil de tutor cargado exitosamente.",
+  });
+};
 
 module.exports = {
-    createTutor
-}
+  createTutor,
+  viewTutor,
+};
--- src/routes/tutor.routes.js
+++ src/routes/tutor.routes.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Importando middlewares
-const { verifyToken } = require('../middlewares/verifyToken');
+const { verifyToken } = require("../middlewares/verifyToken");
 // Importando controladores
-const TutorCtrl = require('../controllers/tutor.controller');
+const TutorCtrl = require("../controllers/tutor.controller");
 
 // Ruta para crear el perfil del tutor
-router.post('/:id', verifyToken, TutorCtrl.createTutor);
+router.post("/:id", verifyToken, TutorCtrl.createTutor);
+// Ruta para mostrar perfil de tutor
+router.get("/view/:id", TutorCtrl.viewTutor);
 
 module.exports = router;
