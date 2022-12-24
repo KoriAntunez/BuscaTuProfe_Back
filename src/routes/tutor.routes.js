642939359c0a596f3bc5dfeb8b6d6ca68bf52e43
Mishell Gomez <mishell.gomez@unmsm.edu.pe>
2022-12-24 09:00:39 -0500

    Update tutor.routes.js

--- src/routes/tutor.routes.js
+++ src/routes/tutor.routes.js
@@ -8,8 +8,13 @@ const TutorCtrl = require("../controllers/tutor.controller");
 
 // Ruta para crear el perfil del tutor
 router.post("/:id", TutorCtrl.createTutor);
+<<<<<<< Updated upstream
 // Ruta para buscar perfil de tutor
 router.get("/view/:id", TutorCtrl.viewTutor);
+=======
+// Ruta para mostrar perfil de tutor
+router.get("/views/:id", TutorCtrl.viewTutor);
+>>>>>>> Stashed changes
 // Ruta para listar todos los tutores
 router.get("/", TutorCtrl.listTutors);
 
